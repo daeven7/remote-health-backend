c8b9a9b60e8228f4bdc34bc47d952a7ff1ffe1e4
daeven <daevenbarreto7@gmail.com>
2024-10-16 00:36:06 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-
 import express from "express";
 import answer from "./helper.js";
 // import dotenv from 'dotenv';
 
 // dotenv.config();
 
-
 const app = express();
 
 app.get("/health", function (req, res) {
   res.send("OK");
 });
-
 
 // app.get("/answer", async function (req, res) {
 //   try {
@@ -23,21 +20,41 @@ app.get("/health", function (req, res) {
 //   }
 // });
 
-app.post('/answer', async function (req, res) {
-    const { question } = req.body; 
-  
-    if (!question) {
-      return res.status(400).send('Question is required');
-    }
-  
-    try {
-      const response = await answer(question); 
-      res.send(response); 
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error fetching answer');
-    }
-  });
+app.post("/answer", async function (req, res) {
+  const { question } = req.body;
+
+  if (!question) {
+    return res.status(400).send("Question is required");
+  }
+
+  try {
+    const response = await answer(question);
+    res.send(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching answer");
+  }
+});
+
+app.post("/answerAdvanced", async function (req, res) {
+  const { state, village, country, question } = req.body; // Extract state, village, country, and question from the body
+
+  // Check if all required fields are provided
+  if (!state || !village || !country || !question) {
+    return res
+      .status(400)
+      .send("All fields (state, village, country, and question) are required.");
+  }
+
+
+  try {
+    const response = await answer(state, village, country, question);
+    res.send(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching answer");
+  }
+});
 
 app.listen(3000, function () {
   console.log("listening on port 3000!");
--- helper.js
+++ helper.js
@@ -1,11 +1,16 @@
 import { ChatGPTAPI } from "chatgpt";
 
-const answer = async (question) => {
+const answer = async (state, village, country, question) => {
   const api = new ChatGPTAPI({
     // apiKey: process.env.OPENAI_API_KEY,
   });
 
-  const res = await api.sendMessage(question);
+  const prompt = `I am a resident the state of ${state}, village of ${village}, country of ${country}, 
+                  I am sick and this is my question and my symptoms: "${question}". Could you diagnose my sickess and also tell me which 
+                  is the nearest hospital/doctor that i can go to for help based on my location?`;
+
+
+  const res = await api.sendMessage(question); of
   console.log(res.text);
 };
 
